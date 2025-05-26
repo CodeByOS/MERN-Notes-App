bc4c513573a5d7ddb6513e7d8b1749d2821b5182
Oussama <saidi.oussama2004@gmail.com>
2025-05-26 20:05:38 +0100

    Get all Notes Controller..

--- backend/controllers/noteControllers.js
+++ backend/controllers/noteControllers.js
@@ -1,7 +1,17 @@
 const Note = require("../models/Note")
 
 //* Get All Notes..
-const getAllNotes = () => {}
+const getAllNotes = async (req, res) => {
+    try {
+        const notes = await Note.find();
+        if(!notes) return res.status(404).json({ message: "Failed to fetch Notes..!" });
+
+        res.status(200).json(notes);
+    } catch (err) {
+        console.error("Error in getAllNotes Controller..!");
+        res.status(500).json({ message: "Internal Server Error..!" });
+    }
+}
 
 //* Create Note..
 const createNote = () => {}

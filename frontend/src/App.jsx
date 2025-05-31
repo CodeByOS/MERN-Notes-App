920d50088810c5136140f508c927132e4c5ca312
Oussama <saidi.oussama2004@gmail.com>
2025-05-31 19:17:11 +0100

    CreatePage Component..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,24 @@
 import { Routes, Route } from 'react-router'
-import  HomePage from './pages/HomePage'
+import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import NoteDetailPage from './pages/NoteDetailPage'
 import NotFoundPage from './pages/NotFoundPage'
-// import { toast } from 'react-hot-toast'
+import Navbar from './components/Navbar'
+
 const App = () => {
   return (
-    <div data-theme="business">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreatePage />} />
-        <Route path="/note/:id" element={<NoteDetailPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+    <div className="min-h-screen bg-base-100 text-base-content">
+      {/* Navbar */}
+      <Navbar />
+      {/* Main Content */}
+      <main className="container mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/note/:id" element={<NoteDetailPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
     </div>
   )
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -34,7 +34,6 @@ const HomePage = () => {
 
   return (
     <div className="min-h-screen bg-base-100">
-      <Navbar />
 
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {isRateLimited ? (
